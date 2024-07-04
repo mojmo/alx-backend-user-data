e0d83faaa57b1564913c28a46bc0c2a9110c71e4
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-05 00:47:46 +0300

    fix linting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,10 +41,16 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Initializes a RedactingFormatter object.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats a log record.
+        """
         original_message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, original_message,
                             self.SEPARATOR)
