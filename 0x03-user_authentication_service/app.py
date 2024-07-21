c2e9a71e9df0ac176a8f4f7587c6ed2228f911ba
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-21 22:36:38 +0300

    handle profile route to retrieve the user's profile information.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,5 +91,27 @@ def logout() -> Union[redirect, abort]:
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> Union[jsonify, abort]:
+    """
+    Retrieve the user's profile information.
+
+    Returns:
+        Union[jsonify, abort]: A JSON response with the user's email if the
+        session ID is valid, otherwise an abort with a 403 HTTP status.
+    """
+    session_id = request.cookies.get('session_id')
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
