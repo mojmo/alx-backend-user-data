0a16a497ccebb22e0a68a721a1358824ee016657
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-21 18:53:45 +0300

    implement the end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,15 +9,35 @@ It demonstrates how to create a basic JSON API endpoint using Flask.
 The app responds to GET requests at the root URL with a JSON message.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def welcome() -> str:
     """Route handler for the root URL."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> tuple:
+    """
+    Register a new user.
+
+    Returns:
+        tuple: A tuple containing the JSON response and the HTTP status code.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
