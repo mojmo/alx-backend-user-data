a940eaae5a3fa15b384cfca3ac1cb12ddb417a93
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-21 20:30:43 +0300

    implement login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,7 +9,7 @@ It demonstrates how to create a basic JSON API endpoint using Flask.
 The app responds to GET requests at the root URL with a JSON message.
 """
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -38,6 +38,32 @@ def register_user() -> tuple:
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    Log in a user.
+
+    Expects form data with 'email' and 'password' fields.
+    If the login information is incorrect, returns a 401 status.
+    Otherwise, creates a new session for the user, sets a 'session_id' cookie,
+    and returns a 200 status with a JSON payload containing the user's email
+    and a 'logged in' message.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(
+            jsonify({"email": email, "message": "logged in"}),
+            200
+        )
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
